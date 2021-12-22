854f7c754d8978f9f9da746da91d21b74eef3cfb
bbbearman <bearman.m@qq.com>
2021-12-22 11:31:20 +0800

    okkk

---
+++ UserInterface/button_action.py
@@ -0,0 +1,155 @@
+from typing import ChainMap, get_args
+from PyQt5 import QtCore, QtGui, QtWidgets
+import can
+import cantools
+
+
+#-------------------------------------------------#
+#---------------下拉选择按钮响应函数-----------------#
+#-------------------------------------------------#
+def msg_select_action(mywindow,channel):
+    canchannel = channel[0:-2]
+    comboBox_attr = getattr(mywindow, "comboBox_"+channel,None )
+    selected_msgname = comboBox_attr.currentText()                         #获取当前选中的messgae
+    if canchannel == "ch1":
+        selected_msg = mywindow.db1.get_message_by_name(selected_msgname)
+    else:
+        selected_msg = mywindow.db2.get_message_by_name(selected_msgname)
+    
+    #cycle time 更新显示
+    try:
+        cycletime = str(selected_msg.cycle_time)
+    except:
+        cycletime = '0'
+
+    cycletime_attr = getattr(mywindow,"cycletime_"+channel,None)
+    cycletime_attr.setText(cycletime)
+    
+    datafiled_refresh_save_attr = getattr(mywindow,"datafiled_refresh_save_"+channel,None)
+    datafiled_refresh_save_attr.setText('Edit')  #切换message的时候，重新开启编辑
+    
+
+    setattr(mywindow,"packedmsg_"+channel,b'\x00\x00\x00\x00\x00\x00\x00\x00')
+    setattr(mywindow,"editflag_"+canchannel,0)     # 1表示有人正在编辑状态，还未保存
+
+
+#-------------------------------------------------#
+#---------------保存更改按钮响应---------------------#
+#-------------------------------------------------#
+def msg_encode_action(mywindow,channel):
+    canchannel = channel[0:-2]
+    comboBox_attr = getattr(mywindow, "comboBox_"+channel,None )
+    selected_msgname = comboBox_attr.currentText()                         #获取当前选中的messgae
+    if canchannel == "ch1":
+        selected_msg = mywindow.db1.get_message_by_name(selected_msgname)
+    else:
+        selected_msg = mywindow.db2.get_message_by_name(selected_msgname)
+    
+
+    datafiled_refresh_save_attr = getattr(mywindow,"datafiled_refresh_save_"+channel,None)
+    text = datafiled_refresh_save_attr.text()
+    #text = mywindow.datafiled_refresh_save_ch1_1.text()
+    flag = 0
+
+    if text == 'Edit'and getattr(mywindow,"editflag_"+canchannel,None) == 0: #如果有其他栏正在编辑，不能进入
+        mywindow.signal_edit_window.clear()  #先清空屏幕
+
+        #decoded_dict = mywindow.db1.decode_message(selected_msg.frame_id, mywindow.packedmsg_ch1_1,decode_choices=False)
+        
+        if canchannel == "ch1":
+            decoded_dict = mywindow.db1.decode_message(selected_msg.frame_id, getattr(mywindow,"packedmsg_"+channel),decode_choices=False)
+        if canchannel == "ch2":
+            decoded_dict = mywindow.db2.decode_message(selected_msg.frame_id, getattr(mywindow,"packedmsg_"+channel),decode_choices=False)
+
+        
+        for key in decoded_dict.keys():
+            mywindow.signal_edit_window.appendPlainText(key+" : "+str(decoded_dict[key]))
+        flag = 1
+        mywindow.terminal.append('[info] Editing in ch1')
+        
+        setattr(mywindow,"editflag_"+canchannel,1)
+
+
+    if text == 'Save':
+        tt = mywindow.signal_edit_window.toPlainText().split('\n')
+        signal_dict = {}
+        
+        for item in tt:   
+            signal_dict[item.split(":")[0].strip(" ")] = float(item.split(":")[1])
+            #print(type(item.split(":")[0]),type(float(item.split(":")[1])),type(1.1))
+
+        try:    
+            setattr(mywindow, "packedmsg_"+channel,selected_msg.encode(signal_dict))  #报文打包
+            
+            datafield_overview_attr = getattr(mywindow, "datafield_overview_"+channel, None)
+            datafield_overview_attr.setText(str(getattr(mywindow, "packedmsg_"+channel).hex('-')))  #打包好的报文显示
+            #mywindow.datafield_overview_ch1_1.setText(str(mywindow.packedmsg_ch1_1.hex('-')))        
+        except:
+            mywindow.terminal.append('[error] Error in encode in ch1, try again!')
+        else:
+            mywindow.terminal.append('[info] Encode completely in ch1')
+            
+            setattr(mywindow,"editflag_"+canchannel, 0)
+            
+            flag = 2
+    
+    if flag == 1:
+        datafiled_refresh_save_attr.setText('Save')
+        datafiled_refresh_save_attr.setStyleSheet("color: red")
+    elif flag == 2:
+        datafiled_refresh_save_attr.setText('Edit')
+        datafiled_refresh_save_attr.setStyleSheet("color: black")
+    flag = 0
+    
+
+#-------------------------------------------------#
+#---------------发送一次按钮响应---------------------#
+#-------------------------------------------------#
+def send_action_once(mywindow,channel):
+    canchannel = channel[0:-2]
+    checkbox_attr = getattr(mywindow, "checkbox_"+channel, None)
+    comboBox_attr = getattr(mywindow, "comboBox_"+channel, None)
+
+    if not checkbox_attr.isChecked():
+        try:
+            selected_msgname = comboBox_attr.currentText()               #获取当前选中的message
+
+            if canchannel == "ch1":
+                selected_msg = mywindow.db1.get_message_by_name(selected_msgname)
+            else:
+                selected_msg = mywindow.db2.get_message_by_name(selected_msgname)
+
+            mywindow.terminal.append('[info] send once '+ str(hex(selected_msg.frame_id)) +"  "+  str(getattr(mywindow, "packedmsg_"+channel).hex('-')) )
+        except:
+            mywindow.terminal.append('[error] please verify the data you want to send!')
+    else:
+        mywindow.terminal.append('[warning] cannot send once when this message sending cyclicly!')
+
+
+#-------------------------------------------------#
+#---------------循环发送按钮响应---------------------#
+#-------------------------------------------------#
+def send_action_cyclic(mywindow,channel):
+    canchannel = channel[0:-2]
+    checkbox_attr = getattr(mywindow, "checkbox_"+channel, None)
+    comboBox_attr = getattr(mywindow, "comboBox_"+channel, None)
+    cycletime_attr = getattr(mywindow, "cycletime_"+channel, None)
+
+    if checkbox_attr.isChecked():
+        try:
+            selected_msgname = comboBox_attr.currentText()               #获取当前选中的message
+            if canchannel == "ch1":
+                selected_msg = mywindow.db1.get_message_by_name(selected_msgname)
+            else:
+                selected_msg = mywindow.db2.get_message_by_name(selected_msgname)
+            
+            mywindow.terminal.append('[info] send cyclic '+ str(hex(selected_msg.frame_id)) +" "+ cycletime_attr.text()+"ms  "+  str(getattr(mywindow, "packedmsg_"+channel).hex('-')) )
+        except:
+            mywindow.terminal.append('[error] please verify the data you want to send!')
+    else:
+        mywindow.terminal.append('[info] stop send cyclic message now')
+
+
+
+
+
--- UserInterface/ui_handler.py
+++ UserInterface/ui_handler.py
@@ -9,7 +9,7 @@ import can
 import cantools
 
 
-import utils
+import button_action
 
 
 class mywindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -34,14 +34,13 @@ class mywindow(QtWidgets.QMainWindow, Ui_MainWindow):
             return 0 #对话框如果意外终止，直接退出
 
         try:
-            db1 = cantools.database.load_file(dbc_filepath1) #对dbc进行解析
-            self.db1 = db1
-            msglist_1 = dbc_msg_list(db1)
-            self.comboBox_ch1_1.addItems(msglist_1);self.comboBox_ch1_1.setCurrentIndex(-1)
-            self.comboBox_ch1_2.addItems(msglist_1);self.comboBox_ch1_2.setCurrentIndex(-1)
-            self.comboBox_ch1_3.addItems(msglist_1);self.comboBox_ch1_3.setCurrentIndex(-1)
-            self.comboBox_ch1_4.addItems(msglist_1);self.comboBox_ch1_4.setCurrentIndex(-1)
-            self.comboBox_ch1_5.addItems(msglist_1);self.comboBox_ch1_5.setCurrentIndex(-1)
+            self.db1 = cantools.database.load_file(dbc_filepath1) #对dbc进行解析
+            msglist = dbc_msg_list(self.db1)
+            self.comboBox_ch1_1.addItems(msglist);self.comboBox_ch1_1.setCurrentIndex(-1)
+            self.comboBox_ch1_2.addItems(msglist);self.comboBox_ch1_2.setCurrentIndex(-1)
+            self.comboBox_ch1_3.addItems(msglist);self.comboBox_ch1_3.setCurrentIndex(-1)
+            self.comboBox_ch1_4.addItems(msglist);self.comboBox_ch1_4.setCurrentIndex(-1)
+            self.comboBox_ch1_5.addItems(msglist);self.comboBox_ch1_5.setCurrentIndex(-1)
             
         except:
             self.text_dbc1.setText('file fromat error, plz select again') #获取到的绝对路径显示在路径框中
@@ -63,17 +62,21 @@ class mywindow(QtWidgets.QMainWindow, Ui_MainWindow):
             return 0 #对话框如果意外终止，直接退出
 
         try:
-            db2 = cantools.database.load_file(dbc_filepath2) #对dbc进行解析
-            self.db2 = db2
-            msglist_2 = dbc_msg_list(db2)
+            self.db2 = cantools.database.load_file(dbc_filepath2) #对dbc进行解析
+            msglist = dbc_msg_list(self.db2)
+            self.comboBox_ch2_1.addItems(msglist);self.comboBox_ch2_1.setCurrentIndex(-1)
+            self.comboBox_ch2_2.addItems(msglist);self.comboBox_ch2_2.setCurrentIndex(-1)
+            self.comboBox_ch2_3.addItems(msglist);self.comboBox_ch2_3.setCurrentIndex(-1)
+            self.comboBox_ch2_4.addItems(msglist);self.comboBox_ch2_4.setCurrentIndex(-1)
+            self.comboBox_ch2_5.addItems(msglist);self.comboBox_ch2_5.setCurrentIndex(-1)
             
         except:
             self.text_dbc2.setText('file fromat error, plz select again') #获取到的绝对路径显示在路径框中
             self.flag_OK2.setText('Error!')
-            self.terminal.append( now + 'dbc2 file format error, please select again')
+            self.terminal.append( '[error] dbc1 file format error, please select again')
         else:
             self.flag_OK2.setText('dbc loaded!')
-            self.terminal.append(now + 'dbc1 loaded')        
+            self.terminal.append('[info] dbc1 loaded')   
         
 
 
@@ -82,76 +85,55 @@ class mywindow(QtWidgets.QMainWindow, Ui_MainWindow):
     #-------------------------------------------------#
     
     def msg_select_action_ch1_1(self):
-        utils.msg_select_action(self,"ch1_1")         
+        button_action.msg_select_action(self,"ch1_1")         
     def msg_select_action_ch1_2(self):
-        utils.msg_select_action(self,"ch1_2") 
+        button_action.msg_select_action(self,"ch1_2") 
     def msg_select_action_ch1_3(self):
-        utils.msg_select_action(self,"ch1_3") 
+        button_action.msg_select_action(self,"ch1_3") 
     def msg_select_action_ch1_4(self):
-        utils.msg_select_action(self,"ch1_4") 
+        button_action.msg_select_action(self,"ch1_4") 
     def msg_select_action_ch1_5(self):
-        utils.msg_select_action(self,"ch1_5")
+        button_action.msg_select_action(self,"ch1_5")
     #-------------------------------------------------#
     #---------------保存更改按钮响应---------------------#
     #-------------------------------------------------#
 
     def msg_encode_action_ch1_1(self):
-        utils.msg_encode_action(self,"ch1_1")
-
+        button_action.msg_encode_action(self,"ch1_1")
     def msg_encode_action_ch1_2(self):
-        utils.msg_encode_action(self,"ch1_2")
-
+        button_action.msg_encode_action(self,"ch1_2")
     def msg_encode_action_ch1_3(self):
-        utils.msg_encode_action(self,"ch1_3")
-
+        button_action.msg_encode_action(self,"ch1_3")
     def msg_encode_action_ch1_4(self):
-        utils.msg_encode_action(self,"ch1_4")
-
+        button_action.msg_encode_action(self,"ch1_4")
     def msg_encode_action_ch1_5(self):
-        utils.msg_encode_action(self,"ch1_5")
+        button_action.msg_encode_action(self,"ch1_5")
 
     #-------------------------------------------------#
     #------------------发送一次按钮---------------------#
     #-------------------------------------------------#
 
     def send_action_once_ch1_1(self):
-        utils.send_action_once(self,"ch1_1")
+        button_action.send_action_once(self,"ch1_1")
     def send_action_once_ch1_2(self):
-        utils.send_action_once(self,"ch1_2")
+        button_action.send_action_once(self,"ch1_2")
     def send_action_once_ch1_3(self):
-        utils.send_action_once(self,"ch1_3")
+        button_action.send_action_once(self,"ch1_3")
     def send_action_once_ch1_4(self):
-        utils.send_action_once(self,"ch1_4")
+        button_action.send_action_once(self,"ch1_4")
     def send_action_once_ch1_5(self):
-        utils.send_action_once(self,"ch1_5")
+        button_action.send_action_once(self,"ch1_5")
 
     #-------------------------------------------------#
     #------------------持续发送按钮---------------------#
     #-------------------------------------------------#
     def send_action_cyclic_ch1_1(self):
-        if self.checkbox_ch1_1.isChecked():
-            try:
-                selected_msgname = self.comboBox_ch1_1.currentText()               #获取当前选中的message
-                selected_msg = self.db1.get_message_by_name(selected_msgname)
-                self.terminal.append('[info] send cyclic '+ str(hex(selected_msg.frame_id)) +" "+ self.cycletime_ch1_1.text()+"ms  "+  str(self.packedmsg_ch1_1.hex('-')) )
-            except:
-                self.terminal.append('[error] please verify the data you want to send!')
-        else:
-            self.terminal.append('[info] stop send cyclic message now')
-
+        button_action.send_action_cyclic(self, "ch1_1")
     def send_action_cyclic_ch1_2(self):
-        if self.checkbox_ch1_2.isChecked():
-            try:
-                selected_msgname = self.comboBox_ch1_2.currentText()               #获取当前选中的message
-                selected_msg = self.db1.get_message_by_name(selected_msgname)
-                self.terminal.append('[info] send cyclic '+ str(hex(selected_msg.frame_id)) +" "+ self.cycletime_ch1_2.text()+"ms  "+  str(self.packedmsg_ch1_2.hex('-')) )
-            except:
-                self.terminal.append('[error] please verify the data you want to send!')
-        else:
-            self.terminal.append('[info] stop send cyclic message now')
+        button_action.send_action_cyclic(self, "ch1_2")
     def send_action_cyclic_ch1_3(self):
-        pass
+        button_action.send_action_cyclic(self, "ch1_3")
     def send_action_cyclic_ch1_4(self):
-        pass
+        button_action.send_action_cyclic(self, "ch1_4")
     def send_action_cyclic_ch1_5(self):
-        pass
+        button_action.send_action_cyclic(self, "ch1_5")
