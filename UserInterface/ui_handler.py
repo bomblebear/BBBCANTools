0470700a131a6dc3fae8f3139643d1b912a43fd1
bbbearman <bearman.m@qq.com>
2021-12-22 10:47:21 +0800

    重构了按钮响应的函数框架

--- UserInterface/ui_handler.py
+++ UserInterface/ui_handler.py
@@ -9,6 +9,9 @@ import can
 import cantools
 
 
+import utils
+
+
 class mywindow(QtWidgets.QMainWindow, Ui_MainWindow):
     
     def __init__(self):
@@ -77,181 +80,50 @@ class mywindow(QtWidgets.QMainWindow, Ui_MainWindow):
     #-------------------------------------------------#
     #-----------------下拉选择按钮响应-------------------#
     #-------------------------------------------------#
-
+    
     def msg_select_action_ch1_1(self):
-        selected_msgname = self.comboBox_ch1_1.currentText()
-        selected_msg = self.db1.get_message_by_name(selected_msgname)
-        #cycle time 更新显示
-        try:
-            cycletime = str(selected_msg.cycle_time)
-        except:
-            cycletime = '0'
-        self.cycletime_ch1_1.setText(cycletime)
-        self.datafiled_refresh_save_ch1_1.setText('Edit')  #切换message的时候，重新开启编辑
-        #self.signal_edit_window.clear()                    #清空右侧栏
-
-        self.packedmsg_ch1_1 = b'\x00\x00\x00\x00\x00\x00\x00\x00'
-        self.editflag_ch1 = 0  # 1表示有人正在编辑状态，还未保存
-        
-        
-
+        utils.msg_select_action(self,"ch1_1")         
     def msg_select_action_ch1_2(self):
-        selected_msgname = self.comboBox_ch1_2.currentText()
-        selected_msg = self.db1.get_message_by_name(selected_msgname)
-        #cycle time 更新显示
-        try:
-            cycletime = str(selected_msg.cycle_time)
-        except:
-            cycletime = '0'
-        self.cycletime_ch1_2.setText(cycletime)
-        self.datafiled_refresh_save_ch1_2.setText('Edit')
-        #self.signal_edit_window.clear()
-
-        self.packedmsg_ch1_2 = b'\x00\x00\x00\x00\x00\x00\x00\x00'
-
+        utils.msg_select_action(self,"ch1_2") 
     def msg_select_action_ch1_3(self):
-        pass
+        utils.msg_select_action(self,"ch1_3") 
     def msg_select_action_ch1_4(self):
-        pass
+        utils.msg_select_action(self,"ch1_4") 
     def msg_select_action_ch1_5(self):
-        pass
-
-
+        utils.msg_select_action(self,"ch1_5")
     #-------------------------------------------------#
     #---------------保存更改按钮响应---------------------#
     #-------------------------------------------------#
 
     def msg_encode_action_ch1_1(self):
-        selected_msgname = self.comboBox_ch1_1.currentText()               #获取当前选中的message
-        selected_msg = self.db1.get_message_by_name(selected_msgname)
-        
-        text = self.datafiled_refresh_save_ch1_1.text()
-        flag = 0
-
-        if text == 'Edit'and self.editflag_ch1 == 0: #如果有其他栏正在编辑，不能进入
-            self.signal_edit_window.clear()  #先清空屏幕
-            decoded_dict = self.db1.decode_message(selected_msg.frame_id, self.packedmsg_ch1_1,decode_choices=False)
-            for key in decoded_dict.keys():
-                self.signal_edit_window.appendPlainText(key+" : "+str(decoded_dict[key]))
-            flag = 1
-            self.terminal.append('[info] Editing in ch1')
-            self.editflag_ch1 = 1
-
-        if text == 'Save':
-            tt = self.signal_edit_window.toPlainText().split('\n')
-            signal_dict = {}
-            
-            for item in tt:   
-                signal_dict[item.split(":")[0].strip(" ")] = float(item.split(":")[1])
-                #print(type(item.split(":")[0]),type(float(item.split(":")[1])),type(1.1))
-
-            try:    
-                self.packedmsg_ch1_1 = selected_msg.encode(signal_dict)  #报文打包
-                #print(self.packedmsg_ch1_1.hex('-'))
-                self.datafield_overview_ch1_1.setText(str(self.packedmsg_ch1_1.hex('-')))                #打包好的报文显示
-            except:
-                self.terminal.append('[error] Error in encode in ch1, try again!')
-            else:
-                self.terminal.append('[info] Encode completely in ch1')
-                self.editflag_ch1 = 0
-                flag = 2
-        
-        if flag == 1:
-            self.datafiled_refresh_save_ch1_1.setText('Save')
-            self.datafiled_refresh_save_ch1_1.setStyleSheet("color: red")
-        elif flag == 2:
-            self.datafiled_refresh_save_ch1_1.setText('Edit')
-            self.datafiled_refresh_save_ch1_1.setStyleSheet("color: balck")
-        flag = 0
-        
+        utils.msg_encode_action(self,"ch1_1")
 
     def msg_encode_action_ch1_2(self):
-        selected_msgname = self.comboBox_ch1_2.currentText()               #获取当前选中的message
-        selected_msg = self.db1.get_message_by_name(selected_msgname)
-        
-        text = self.datafiled_refresh_save_ch1_2.text()
-        flag = 0
-
-        if text == 'Edit'and self.editflag_ch1 == 0: #如果有其他栏正在编辑，不能进入
-            self.signal_edit_window.clear()  #先清空屏幕
-            decoded_dict = self.db1.decode_message(selected_msg.frame_id, self.packedmsg_ch1_2,decode_choices=False)
-            for key in decoded_dict.keys():
-                self.signal_edit_window.appendPlainText(key+" : "+str(decoded_dict[key]))
-            flag = 1
-            self.terminal.append('[info] Editing in ch1')
-            self.editflag_ch1 = 1
-
-        if text == 'Save':
-            tt = self.signal_edit_window.toPlainText().split('\n')
-            signal_dict = {}
-            
-            for item in tt:   
-                signal_dict[item.split(":")[0].strip(" ")] = float(item.split(":")[1])
-                #print(type(item.split(":")[0]),type(float(item.split(":")[1])),type(1.1))
-
-            try:    
-                self.packedmsg_ch1_2 = selected_msg.encode(signal_dict)  #报文打包
-                #print(self.packedmsg_ch1_1.hex('-'))
-                self.datafield_overview_ch1_2.setText(str(self.packedmsg_ch1_2.hex('-')))                        #打包好的报文显示
-            except:
-                self.terminal.append('[error] Error in encode in ch1, try again!')
-            else:
-                self.terminal.append('[info] Encode completely in ch1')
-                self.editflag_ch1 = 0
-                flag = 2
-        
-        if flag == 1:
-            self.datafiled_refresh_save_ch1_2.setText('Save')
-            self.datafiled_refresh_save_ch1_2.setStyleSheet("color: red")
-        elif flag == 2:
-            self.datafiled_refresh_save_ch1_2.setText('Edit')
-            self.datafiled_refresh_save_ch1_2.setStyleSheet("color: balck")
-        flag = 0
+        utils.msg_encode_action(self,"ch1_2")
 
     def msg_encode_action_ch1_3(self):
-        pass
+        utils.msg_encode_action(self,"ch1_3")
 
     def msg_encode_action_ch1_4(self):
-        pass
+        utils.msg_encode_action(self,"ch1_4")
 
     def msg_encode_action_ch1_5(self):
-        pass
+        utils.msg_encode_action(self,"ch1_5")
 
     #-------------------------------------------------#
     #------------------发送一次按钮---------------------#
     #-------------------------------------------------#
 
     def send_action_once_ch1_1(self):
-        
-        if not self.checkbox_ch1_1.isChecked():
-            try:
-                selected_msgname = self.comboBox_ch1_1.currentText()               #获取当前选中的message
-                selected_msg = self.db1.get_message_by_name(selected_msgname)
-                self.terminal.append('[info] send once '+ str(hex(selected_msg.frame_id)) +"  "+  str(self.packedmsg_ch1_1.hex('-')) )
-            except:
-                self.terminal.append('[error] please verify the data you want to send!')
-        else:
-            self.terminal.append('[warning] cannot send once when this message sending cyclicly!')
-        
+        utils.send_action_once(self,"ch1_1")
     def send_action_once_ch1_2(self):
-        if not self.checkbox_ch1_2.isChecked():
-            try:
-                selected_msgname = self.comboBox_ch1_2.currentText()               #获取当前选中的message
-                selected_msg = self.db1.get_message_by_name(selected_msgname)
-                self.terminal.append('[info] send once '+ str(hex(selected_msg.frame_id)) +"  "+ str(self.packedmsg_ch1_2.hex('-')) )
-            except:
-                self.terminal.append('[error] please verify the data you want to send!')
-        else:
-            self.terminal.append('[warning] cannot send once when this message sending cyclicly!')
-
+        utils.send_action_once(self,"ch1_2")
     def send_action_once_ch1_3(self):
-        pass
-
+        utils.send_action_once(self,"ch1_3")
     def send_action_once_ch1_4(self):
-        pass
+        utils.send_action_once(self,"ch1_4")
     def send_action_once_ch1_5(self):
-        pass
-
+        utils.send_action_once(self,"ch1_5")
 
     #-------------------------------------------------#
     #------------------持续发送按钮---------------------#
